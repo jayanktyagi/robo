dded069144dc50980dbeca3dd11ca89979ec471c
jayanktyagi <jayanktyagi20@gmail.com>
2021-09-16 10:12:18 +0530

    deployment commit #1

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
+import time
+
 import tweepy
 
 from credentials import consumer_key, consumer_key_secret, access_token, access_token_secret
 
-file = open("tweets.txt", "r")
 
-content = file.read()
-twitlist = content.split("\n")
+def tweet():
+    file = open("tweets.txt", "r")
 
-with open("tweets.txt", "w") as fp:
-    for i, line in enumerate(twitlist):
-        if i == 0:
-            msg = line
-            print(line)
-        if i != 0:
-            fp.write(line+"\n")
+    content = file.read()
+    twitlist = content.split("\n")
 
-file.close()
+    with open("tweets.txt", "w") as fp:
+        for i, line in enumerate(twitlist):
+            if i == 0:
+                msg = line
+                print(line)
+            if i != 0:
+                fp.write(line+"\n")
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
+    file.close()
 
-auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
 
-api = tweepy.API(auth);
+    auth.set_access_token(access_token, access_token_secret)
 
-api.update_status(msg)
+    api = tweepy.API(auth);
 
+    api.update_status(msg)
 
+def main():
+    while True:
+        tweet()
+        time.sleep(43200)
+if __name__ == "__main__":
+    main()
