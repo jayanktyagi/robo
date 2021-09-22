c6a1492e1370654fcdf31dbfce1497196df2a19e
jayanktyagi <jayanktyagi20@gmail.com>
2021-09-22 15:03:09 +0530

    fixed tweet length issue in main.py and added tweets

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import tweepy
 
 from os import environ
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+# CONSUMER_KEY = environ['CONSUMER_KEY']
+# CONSUMER_SECRET = environ['CONSUMER_SECRET']
+# ACCESS_KEY = environ['ACCESS_KEY']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
+import credentials
 
 
 def authenticate():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_key_secret)
 
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    auth.set_access_token(credentials.access_token, credentials.access_token_secret)
 
     api = tweepy.API(auth)
 
@@ -25,30 +26,34 @@ def getLastTweet():
 
     self_id = api.me().id
 
-    fetch_tweet = api.user_timeline(id=self_id, count=1)[0]
-    last_tweet = fetch_tweet.text
+    fetch_tweet = api.user_timeline(id=self_id, count=1, tweet_mode="extended")[0]
+    last_tweet = fetch_tweet.full_text
 
     return last_tweet
 
 
-def tweet():
-    api = authenticate()
+def get_tweet():
     last_tweet = getLastTweet()
     file = open("tweets.txt", "r")
 
     content = file.read()
     tweet_list = content.split("\n")
     idx = 0
+    print(type(last_tweet), last_tweet)
 
     for i, line in enumerate(tweet_list):
-        if line != last_tweet:
-            continue
-        elif line == last_tweet:
+        if line == last_tweet:
             idx = i + 1
 
-    msg = tweet_list[idx]
+    return tweet_list[idx]
+
+
+def tweet():
+    api = authenticate()
+    msg = get_tweet()
+
     print(msg)
-    api.update_status(msg)
+    # api.update_status(msg)
 
 
 def main():
